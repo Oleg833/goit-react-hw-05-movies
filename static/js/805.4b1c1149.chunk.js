1b5464931340f70064342a87787be7d70f55d96e
Oleg833 <56390811+Oleg833@users.noreply.github.com>
2023-05-11 01:51:27 +0000

    Deploying to gh-pages from @ Oleg833/goit-react-hw-05-movies@6760cd0e490cf452b222bb5efd0d02e9dd0e53b3 🚀

--- static/js/805.4b1c1149.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,n,t){t.r(n);var o=t(87),s=t(184);n.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h2",{children:"Oops... nothing found..."}),(0,s.jsx)(o.OL,{to:"/",children:"Back to home"})]})}}}]);
-//# sourceMappingURL=805.4b1c1149.chunk.js.map
